b7b395547310f6794c4f3ba3e6145fd17d2ef08b
Arnav Patel <patelarnav96@gmail.com>
2017-06-27 02:11:27 -0700

    change edit_replymarkup frequency in nextbutton. only change when going to first and last ones

--- news.py
+++ news.py
@@ -303,7 +303,6 @@ def whatNews(bot,update):
             update.message.reply_text(x,reply_markup=inlineNextKeyboard1,parse_mode='HTML')
 
 def nextButton(bot,update):
-    print update
     query = update.callback_query.id
     queryObj = update.callback_query
     queryData = update.callback_query.data
@@ -328,20 +327,20 @@ def nextButton(bot,update):
 
     if str(queryData) == "1":
         userfind.currentIndex = userfind.currentIndex + 1
+    bot.edit_message_text(text=x,chat_id=queryObj.message.chat_id,message_id=mid)
     if userfind.currentIndex == 0:
         keyboard = inlineNextKeyboard1
+        bot.edit_message_reply_markup(chat_id =queryObj.message.chat_id,message_id=mid,reply_markup =keyboard,parse_mode='HTML')
     if userfind.currentIndex == len(userfind.currentList)-1:
         keyboard = inlineNextKeyboard3
-    else:
-        keyboard = inlineNextKeyboard2
+        bot.edit_message_reply_markup(chat_id =queryObj.message.chat_id,message_id=mid,reply_markup =keyboard,parse_mode='HTML')
+
         
 ##    x = "<b>"+userfind.currentList[userfind.currentIndex].values()[1].upper()+"</b>"+"\n\n"+userfind.currentList[userfind.currentIndex].values()[0]+"\n\n"+userfind.currentList[userfind.currentIndex].values()[2]
     x = userfind.currentList[userfind.currentIndex]['url']
 
 
-    bot.edit_message_text(text=x,
-                      chat_id=queryObj.message.chat_id,
-                      message_id=mid)
+
     bot.edit_message_reply_markup(chat_id =queryObj.message.chat_id,message_id=mid,reply_markup =keyboard,parse_mode='HTML')
         
 
