1790bc62709880f022ebd9e639c52761485d528d
Arnav Patel <patelarnav96@gmail.com>
2017-06-27 02:22:55 -0700

    add test send message functionchange where message is updated

--- news.py
+++ news.py
@@ -329,16 +329,16 @@ def nextButton(bot,update):
 
     if str(queryData) == "1":
         userfind.currentIndex = userfind.currentIndex + 1
-    x = userfind.currentList[userfind.currentIndex]['url']
 
-    bot.edit_message_text(text=x,chat_id=queryObj.message.chat_id,message_id=mid)
     if userfind.currentIndex == 0:
         keyboard = inlineNextKeyboard1
         bot.edit_message_reply_markup(chat_id =queryObj.message.chat_id,message_id=mid,reply_markup =keyboard,parse_mode='HTML')
     if userfind.currentIndex == len(userfind.currentList)-1:
         keyboard = inlineNextKeyboard3
         bot.edit_message_reply_markup(chat_id =queryObj.message.chat_id,message_id=mid,reply_markup =keyboard,parse_mode='HTML')
+    x = userfind.currentList[userfind.currentIndex]['url']
 
+    bot.edit_message_text(text=x,chat_id=queryObj.message.chat_id,message_id=mid)
         
 ##    x = "<b>"+userfind.currentList[userfind.currentIndex].values()[1].upper()+"</b>"+"\n\n"+userfind.currentList[userfind.currentIndex].values()[0]+"\n\n"+userfind.currentList[userfind.currentIndex].values()[2]
     return        
